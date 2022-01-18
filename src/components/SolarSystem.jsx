4a0e8f02faffb7b714e826547f76f7e83126a9e8
Adolligit <adelson.onod@gmail.com>
2022-01-17 23:00:58 -0300

    Componente SolarSystem renderizando texto de teste

---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <span>nway</span>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;

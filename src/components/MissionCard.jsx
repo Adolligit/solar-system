73827ca74d1e1d342899136027525056c3320bb1
Adolligit <adelson.onod@gmail.com>
2022-01-19 23:11:49 -0300

    Integrando MissionCard no Missons; elementos criados dinâmicamente no MissionCard (inclusive, o atributo 'data-testid')

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
-    const { name, year, country, destination } = this.props;
-
     return (
-      <div data-testid="mission-card">
-        <span data-testid="mission-name">{ name }</span>
-        <span data-testid="mission-year">{ year }</span>
-        <span data-testid="mission-country">{ country }</span>
-        <span data-testid="mission-destination">{ destination }</span>
-      </div>
+      <li data-testid="mission-card">
+        {
+          Object.entries(this.props)
+            .map((prop) => (
+              <p
+                key={ prop[1] }
+                data-testid={ `mission-${prop[0]}` }
+              >
+                { prop[1] }
+              </p>
+            ))
+        }
+      </li>
     );
   }
 }
-
-MissionCard.propTypes = {
-  name: PropTypes.string.isRequired,
-  year: PropTypes.number.isRequired,
-  country: PropTypes.string.isRequired,
-  destination: PropTypes.string.isRequired,
-};
 
 export default MissionCard;
--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
+  CreateMissions() {
+    return (
+      missions.map((mission) => (
+        <MissionCard
+          key={ mission.name }
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+        />
+      ))
+    );
+  }
+
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ol>
+          { this.CreateMissions() }
+        </ol>
       </div>
     );
   }
